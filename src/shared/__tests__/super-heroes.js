c4295bc9962b713298a921095174be92dbed8f7b
opauloh <paulo.scape@gmail.com>
2020-09-03 01:23:44 -0300

    snapshot vs inline snapshot

--- src/shared/__tests__/super-heroes.js
+++ src/shared/__tests__/super-heroes.js
@@ -1,6 +1,27 @@
-import { getFlyingSuperHeroes} from '../super-heroes';
+import {getFlyingSuperHeroes} from '../super-heroes'
 
-test('return super heroes that can fly', () => {
+test('return super heroes that can fly with snapshot', () => {
   const flyingHeroes = getFlyingSuperHeroes()
-  expect(flyingHeroes).toMatchSnapshot()
+  expect(flyingHeroes).toMatchSnapshot() // Snapshots are good, but the problem altough, is that they can be very long and unreadable...
+});
+test('return super heroes that can fly with inline snapshot', () => {
+  const flyingHeroes = getFlyingSuperHeroes()
+  expect(flyingHeroes).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "name": "Dynaguy",
+        "powers": Array [
+          "laser",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "dri3lis",
+        "powers": Array [
+          "premonition",
+          "fly",
+        ],
+      },
+    ]
+  `)
 })
--- src/shared/super-heroes.js
+++ src/shared/super-heroes.js
@@ -6,6 +6,10 @@ const heroes = [
   {
     name: 'Paulo',
     powers: ['press arms']
+  },
+  {
+    name: 'dri3lis',
+    powers: ['premonition', 'fly']
   }
 ]
 export const getFlyingSuperHeroes = () => heroes.filter(hero => hero.powers.includes('fly'))
