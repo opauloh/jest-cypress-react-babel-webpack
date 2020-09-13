d8792b08925a66aee38e33d5ec5bb0686b122439
opauloh <paulo.scape@gmail.com>
2020-09-13 10:18:31 -0300

    adding a Wrapper component so we dont have to put it always

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,32 +1,45 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'emotion-theming'
+import PropTypes from 'prop-types'
 import CalculatorDisplay from '../calculator-display'
+import { dark } from '../../themes'
+
+function Wrapper({ children }) {
+  return <ThemeProvider theme={dark}>{children}</ThemeProvider>
+}
+
+Wrapper.propTypes = {
+  children: PropTypes.node
+}
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value={'10'} />)
+  const { container, rerender } = render(<CalculatorDisplay value="0" />, {
+    wrapper: Wrapper
+  })
+  rerender(<CalculatorDisplay value='0' />)
   expect(container).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div>
       <div
-        class="emotion-0"
-        id="calculator-display"
+        class="emotion-0 emotion-1"
       >
         <div
           class="autoScalingText"
           data-testid="total"
           style="transform: scale(1,1);"
         >
-          10
+          0
         </div>
       </div>
     </div>
