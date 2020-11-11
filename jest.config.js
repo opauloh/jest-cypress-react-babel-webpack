71fbb6c2e407f5e92ad7d80622c95fb3a6ebab4f
Paulo Henrique <paulo.scape@gmail.com>
2020-11-11 11:56:13 -0300

    adding coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,18 @@ module.exports = {
   ],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 34,
+      branches: 16,
+      functions: 29,
+      lines: 32,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
