7710b6f297f46f27b2b7c3eb05cfd99833bd4dc0
Paulo Henrique <paulo.scape@gmail.com>
2020-11-12 11:23:06 -0300

    configure jest client

--- test/jest.client.js
+++ test/jest.client.js
@@ -2,23 +2,10 @@ const path = require('path')
 
 module.exports = {
   ...require('./jest-common'),
+  displayName: 'client',
   coverageDirectory: path.join(__dirname, '../coverage/server'),
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/**/*.js'],
   snapshotSerializers: ['jest-emotion'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  coverageThreshold: {
-    global: {
-      statements: 34,
-      branches: 16,
-      functions: 29,
-      lines: 32,
-    },
-    './src/shared/utils.js': {
-      statements: 100,
-      branches: 80,
-      functions: 100,
-      lines: 100,
-    },
-  },
 }
