55dc0f3ad63b70abbe133197db15b5607687c763
Paulo Henrique <paulo.scape@gmail.com>
2020-11-12 11:22:56 -0300

    jest config with projects

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ...require('./test/jest-common'),
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 34,
+      branches: 16,
+      functions: 29,
+      lines: 32,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  projects: ['./test/jest.client.js', './test/jest.server.js'],
+}
